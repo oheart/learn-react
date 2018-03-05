16e110f35718564a8377870ed6e42642eb9aecba
oheart <2353769123@qq.com>
2018-03-05 20:29:25 +0800

    modify name

--- practice/react-cnode/src/components/common/Loading.js
+++ practice/react-cnode/src/components/common/Loading.js
@@ -1,25 +1,24 @@
 import React, {Component} from 'react'
 
-
 class Loading extends Component{
     render(){
     //    const {showLoading} = this.props;
     //    if(!showLoading) return null;
        return (
-        <div className="spinner">
-            <div className="spinner-container container1">
+        <div className="loading-wrapper">
+            <div className="loading-container container1">
                 <div className="circle1"></div>
                 <div className="circle2"></div>
                 <div className="circle3"></div>
                 <div className="circle4"></div>
             </div>
-            <div className="spinner-container container2">
+            <div className="loading-container container2">
                 <div className="circle1"></div>
                 <div className="circle2"></div>
                 <div className="circle3"></div>
                 <div className="circle4"></div>
             </div>
-            <div className="spinner-container container3">
+            <div className="loading-container container3">
                 <div className="circle1"></div>
                 <div className="circle2"></div>
                 <div className="circle3"></div>
